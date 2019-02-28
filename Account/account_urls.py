ec2b6fab402de7e43e4d6046e68375aab7547a4e
Nikita Surnachev <nikita.surnachev03@gmail.com>
2019-03-01 00:35:45 +0300

    Menu

--- Account/account_urls.py
+++ Account/account_urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='account'),
+    path('encpass', views.encPasswd, name='encPasswd'),
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HashPassword
 
 def index(request):
     return render(request, "Account/index.html")
+
+def encPasswd(request):
+    if 'passwd' not in request.GET:
+        return HttpResponse("Incorrect GET request")
+    return HttpResponse(HashPassword(request.GET['passwd']))
